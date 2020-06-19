7727baf6f60b8bcbd8cff62494d2549d7099411b
Ruta98 <anilarut@gmail.com>
2020-06-19 04:30:13 +0300

    Added Moq and DataBase

---
+++ AttendanseControl/AttendanseControl/Data/DAL.Tests/TestUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Tests
+{
+    class TestUserRepository : BaseRepository<User>
+    {
+        public TestUserRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+    }
+}
